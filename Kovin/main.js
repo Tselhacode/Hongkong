a4283d817321f65c9892a3bac58eceee61f6dc48
TselhaCode <68340816+Tselhacode@users.noreply.github.com>
2022-08-10 07:53:06 -0400

    Add files via upload

---
+++ Kovin/main.js
@@ -0,0 +1,39 @@
+$('.one-bubble').click(function(){
+    $('.one-bubble').fadeOut(500);
+    $('.text-exploited-labour').show();
+    $('.text-intro').hide();
+    
+})
+
+$('.two-bubble').click(function(){
+    $('.two-bubble').fadeOut(500);
+    $('.starving-people').show();
+    $('.text-exploited-labour').hide();
+    
+})
+
+$('.three-bubble').click(function(){
+    $('.three-bubble').fadeOut(500);
+    
+})
+
+$('.four-bubble').click(function(){
+    $('.four-bubble').fadeOut(500);
+    
+})
+
+$('.five-bubble').click(function(){
+    $('.five-bubble').fadeOut(500);
+    
+})
+
+$('.six-bubble').click(function(){
+    $('.six-bubble').fadeOut(500);
+    
+})
+
+$('.next').click(function(){
+    $('.flex2').hide();
+    $('.flex4').show();
+})
+
